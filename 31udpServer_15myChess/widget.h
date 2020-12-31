ca79b46e33bd9d0b9bcb8b6e640c66135a9fec7a
purple910 <2018077362@qq.com>
2020-12-31 19:24:40 +0800

    exe

---
+++ 31udpServer_15myChess/widget.h
@@ -0,0 +1,48 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QUdpSocket>
+#include <QHostAddress>
+#include <QDebug>
+#include <QHostInfo>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+struct member
+{
+   QHostAddress addr;
+   quint16 port;
+   QString name;
+};
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_btn_bind_clicked();
+
+    void on_btn_delete_clicked();
+
+    void doProcessReadyRead();
+    void doProcessError(QAbstractSocket::SocketError);
+
+private:
+    Ui::Widget *ui;
+    QUdpSocket *mySocket;
+    QList<member> listClient;
+
+    QString myName;
+    QString toName;
+
+    void Init();
+
+};
+#endif // WIDGET_H
